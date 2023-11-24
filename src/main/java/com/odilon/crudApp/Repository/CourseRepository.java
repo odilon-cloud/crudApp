a0d0dd9ca37032d549a53eb728ac1933af2dfdbe
odilon-cloud <oirambna@gmail.com>
2023-11-24 18:30:43 +0200

    final commit

--- src/main/java/com/odilon/crudApp/Repository/CourseRepository.java
+++ src/main/java/com/odilon/crudApp/Repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.odilon.crudApp.Repository;
 
 import com.odilon.crudApp.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,8 +11,22 @@ import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, String>{
 
-	List<Course> findBySemesterAndAcademicUnitCode(String selectedSemesterId, String selectedDepartmentId);
-
-	List<Course> findByStudentRegNo(String selectedStudentId);
-
+	@Query("SELECT c.courseDefinition.name, c.courseDefinition.course_code, c.courseDefinition.description, t.name, t.assistant_tutor " +
+			"FROM Course c " +
+			"JOIN c.semesterInfo sem " +
+			"JOIN c.academicUnit au " +
+			"JOIN c.teacher t " +
+			"WHERE sem.name = :semesterName " +
+			"AND au.name = :academicUnitName")
+	List<Object[]> findCoursesByDepartmentAndSemester(
+			@Param("semesterName") String semesterName,
+			@Param("academicUnitName") String academicUnitName);
+	@Query("SELECT c.courseDefinition.name, c.courseDefinition.course_code, t.name, t.assistant_tutor " +
+			"FROM Course c " +
+			"JOIN c.semesterInfo sem " +
+			"JOIN c.academicUnit au " +
+			"JOIN c.teacher t " +
+			"JOIN c.student s " +
+			"WHERE s.regNo = :studentRegNo")
+	List<Object[]> findCoursesByStudentRegNo(@Param("studentRegNo") String studentRegNo);
 }
--- src/main/java/com/odilon/crudApp/controlers/ListController.java
+++ src/main/java/com/odilon/crudApp/controlers/ListController.java
@@ -1,5 +1,6 @@
 package com.odilon.crudApp.controlers;
 
+import com.odilon.crudApp.Repository.CourseRepository;
 import com.odilon.crudApp.Repository.RegistrationRepository;
 import com.odilon.crudApp.Repository.StudentCourseRepository;
 import com.odilon.crudApp.model.EAcademicUnit;
@@ -19,7 +20,8 @@ public class ListController {
     @Autowired
     private RegistrationRepository registrationRepository;
 
-
+    @Autowired
+    private CourseRepository courseRepository;
 
     @GetMapping("/students-by-semester")
     public ResponseEntity<List<Object[]>> getStudentsBySemester(@RequestParam String semesterName) {
@@ -43,6 +45,23 @@ public class ListController {
         return ResponseEntity.ok(students);
     }
 
+    @GetMapping("/courses-by-department-semester")
+    public ResponseEntity<List<Object[]>> getCoursesByDepartmentAndSemester(
+            @RequestParam String semesterName,
+            @RequestParam String academicUnitName) {
 
+        List<Object[]> courses = courseRepository
+                .findCoursesByDepartmentAndSemester(semesterName, academicUnitName);
+
+        return ResponseEntity.ok(courses);
+    }
+
+    @GetMapping("/courses-by-student-regno")
+    public ResponseEntity<List<Object[]>> getCoursesByStudentRegNo(@RequestParam String studentRegNo) {
+
+        List<Object[]> courses = courseRepository.findCoursesByStudentRegNo(studentRegNo);
+
+        return ResponseEntity.ok(courses);
+    }
 
 }
